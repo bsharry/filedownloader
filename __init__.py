be98c358d547c4a8f2b79bbccb27810b5606f10d
bsharry <maoyan32@hotmail.com>
2021-04-24 19:49:28 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,7 +7,10 @@ from urllib.parse import urlparse
 
 url = open('url.txt','r').read()
 
-res = requests.get(url, stream=True)
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
+res = requests.get(url, stream=True,headers=headers)
 total_length = int(res.headers.get('content-length'))
 if 'content-disposition' in res.headers:
     d = res.headers['content-disposition']
@@ -18,7 +21,7 @@ else:
     
 fpath = f'download/{fname}'
 
-with open("fpath", "wb") as file:
+with open(fpath, "wb") as file:
     for chunk in progress.bar(res.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1, width=100):
         if chunk:
             file.write(chunk)
