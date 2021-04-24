599d2838aeaacf14c01826f8addc8b313931393f
bsharry <maoyan32@hotmail.com>
2021-04-24 19:04:07 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -22,4 +22,4 @@ with open("fpath", "wb") as file:
     for chunk in progress.bar(res.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1, width=100):
         if chunk:
             file.write(chunk)
-logging.info(f"file {fname} download complete")
+print(f"file {fname} download complete")
